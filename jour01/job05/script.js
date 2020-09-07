7aa36674d5b37d8e7e2977bbbddf4a88cd763e46
remy-inguanez <55872961+remy-inguanez@users.noreply.github.com>
2020-09-07 13:28:26 +0200

    Add files via upload

---
+++ jour01/job05/script.js
@@ -0,0 +1,12 @@
+
+function afficherjourssemaines(tab)
+{
+	for(i=0;i < tab.length;i++)
+	{
+		console.log(tab[i])
+	}
+}
+
+var jourssemaines = ['lundi', 'Mardi','mercredi','jeudi','vendredi','samedi','dimanche'];
+
+afficherjourssemaines(jourssemaines);
