bb50bd4852a570e272f59fd5cc10c53f7b6e366f
remy-inguanez <55872961+remy-inguanez@users.noreply.github.com>
2020-09-14 15:57:25 +0200

    Add files via upload

---
+++ jour06/job03/index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>job03</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+	<section class="section">AL</section>
+	<section class="section">LER</section>
+	<section class="section">L'</section>
+	<section class="section">OM !!</section>
+</body>
+
+</html>
