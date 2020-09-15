c132715a52008f06f3833c504d84a8d43a4fc75d
remy-inguanez <55872961+remy-inguanez@users.noreply.github.com>
2020-09-15 14:56:23 +0200

    Add files via upload

---
+++ jour07/job01/index.php
@@ -0,0 +1,138 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<title>Jour07-Job01</title>
+</head>
+
+<body>
+<header>
+<nav class="nav">
+  <h1 class="align-middle">Lftp</h1>
+  <a class="nav-link active align-middle" href="">Accueil</a>
+  <a class="nav-link disabled align-middle" href="#">Unit</a>
+  <a class="nav-link disabled align-middle" href="#">Job</a>
+  <a class="nav-link disabled align-middle" href="#">Skills</a>
+</nav>
+</header>
+<div class="container">
+	<h1 class="text-center">LaPlateforme_</h1>
+</div>
+<div class="ml-3 row">
+	<div class="row ml-1 col-lg-2">
+		<img class="col-12 " src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSPoHgGtZDWZQBzKBT8ix8Py-55hwexFshjjkxzSfcBjjGI0bcv&usqp=CAU">
+		<b>Un Papillon</b>
+		<p>Un papillon, c'est un peu comme une chenille avec des ailes, ne pas ingérer.</p>
+		<button class="btn btn-primary" >Commander votre papillon</button>
+	</div>
+	<div class="row ml-1 col-lg-8 ">
+		<h1 class="col-12 text-center">Bonjour, le monde!</h1>
+		
+			<p class="container col-12">
+il existe plusieur vision du terme:
+le monde est la matière, l'espace et les phénomemène qui nous sont accéssible par les sens,l'experience ou la raison.
+le sens le plus courant désigne notre planète, la Terre,avec ses habitants et son environement plus ou moin naturel.
+			</p>
+		<b class="col-12">Le sens étendu désigne l'univers dans son ensemble.</b>
+		<div class="col-12">
+			<button class="btn btn-danger">Rebooter le monde</button>
+			<div class="spinner-border text-info" role="status">
+ 				<span class="sr-only">Chargement...</span>
+ 			</div>
+		</div>
+		<div class="col-12 row justify-content-end">
+		
+				<button class="btn btn-link"><</button>
+				<button class="btn btn-link">1</button>
+				<button class="btn btn-link">2</button>
+				<button class="btn btn-link">3</button>
+				<button class="btn btn-link">></button>
+		
+		</div>
+	</div>
+	
+<div class="list-group col-lg-2 ml-1">
+  <a href="#" class="list-group-item list-group-item-action active">Limbes</a>
+  <a href="#" class="list-group-item list-group-item-action">Luxure</a>
+  <a href="#" class="list-group-item list-group-item-action">Gourmandise</a>
+  <a href="#" class="list-group-item list-group-item-action">Avarice</a>
+  <a href="#" class="list-group-item list-group-item-action">Colère</a>
+  <a href="#" class="list-group-item list-group-item-action">Hérésie</a>
+  <a href="#" class="list-group-item list-group-item-action">Violence</a>
+  <a href="#" class="list-group-item list-group-item-action">Ruse et tromperie</a>
+  <a href="#" class="list-group-item list-group-item-action">Trahison</a>
+  <a href="#" class="list-group-item list-group-item-action">Internet explorer</a>
+  
+</div>
+	
+</div>
+<div class="container">
+	<h3 class="col-12 row justify-content-end">Installation de AL 9000</h3>
+	<div class="progress">
+ 		 <div class="progress-bar-animated progress-bar-striped bg-warning" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+	</div>
+</div>
+<section class="container">
+	<div class="row justify-content-between">
+	<form class="col-lg-5">
+		<h3>Recevez votre copie gratuite d'internet 2</h3>
+		
+      		<div class="input-group m-3">
+        		<div class="input-group-prepend">
+        		  <span class="input-group-text" id="inputGroupPrepend3">@</span>
+       			</div>
+        		<input type="text" class="form-control" id="login" placeholder="Email" aria-describedby="inputGroupPrepend3" required>
+      		</div>
+      		<div class="input-group m-3">
+        		
+        		<input type="text" class="form-control" id="mdp" placeholder="Mot De Passe" aria-describedby="inputGroupPrepend3" required>
+        		<div class="input-group-prepend">
+        		  <span class="input-group-text" id="inputGroupPrepend3">@exemple.com</span>
+       			</div>
+      		</div>
+      		<p>URL des Internets 2 et 2.1 Beta</p>
+      		<div class="input-group m-3">
+        		<div class="input-group-prepend">
+        		  <span class="input-group-text" id="inputGroupPrepend3">DogeCoin</span>
+       			</div>
+        		<input type="text" class="form-control" id="url" placeholder="" aria-describedby="inputGroupPrepend3" required>
+        		<div class="input-group-prepend">
+        		  <span class="input-group-text" id="inputGroupPrepend3">.00</span>
+       			</div>
+      		</div>
+      		<div class="input-group m-3">
+        		<div class="input-group-prepend">
+        		  <span class="input-group-text" id="inputGroupPrepend3">https://l33t.lptf/dkwb/berlusconimkt/</span>
+       			</div>
+        		<input type="text" class="form-control" id="" placeholder="" aria-describedby="inputGroupPrepend3" required>
+      		</div>
+    	
+	</form>
+	<form class="col-lg-3 ">	
+ 		 <div class="form-group">
+ 		   <label for="exampleInputEmail1">Adresse email</label>
+    		<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Entrer email">
+   		 <small id="emailHelp" class="form-text text-muted">Partager avec personne votre mail</small>
+  		</div>
+  		<div class="form-group">
+  		  <label for="exampleInputPassword1">Mot de passe</label>
+    		<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  		</div>
+  		<div class="form-check">
+    		<input type="checkbox" class="form-check-input" id="exampleCheck1">
+    		<label class="form-check-label" for="exampleCheck1">Check me</label>
+  		</div>
+  		<button type="submit" class="btn btn-primary">Valider</button>
+	</form>
+</div>
+</section>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+
+</html>
