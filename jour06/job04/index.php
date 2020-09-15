ecea602efbfc7ef32f2511c4446ca9249b1c70a5
remy-inguanez <55872961+remy-inguanez@users.noreply.github.com>
2020-09-15 08:53:52 +0200

    Add files via upload

---
+++ jour06/job04/index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>job04</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+	<header class="menu">
+			<input class="burger" type="checkbox">
+			<nav>
+				<a href="">Accueil</a>
+				<a href="">Présentation</a>
+				<a href="">Contact</a>
+			</nav>
+	</header>
+</body>
+
+</html>
