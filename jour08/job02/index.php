9c202b7ad7e59578f90c3b8713573c1ddc30682f
remy-inguanez <55872961+remy-inguanez@users.noreply.github.com>
2020-09-16 14:27:48 +0200

    Add files via upload

---
+++ jour08/job02/index.php
@@ -0,0 +1,48 @@
+
+ <!DOCTYPE html>
+  <html>
+
+<body>
+
+    <head>
+    <title>Jour08-Job02</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    </head>
+
+      <header class="red darken-1 ">
+        <nav class="container red darken-3 row">
+          <a href="index.php" class="col s3 center red-text text-lighten-4">Accueil</a> <a  class="col s3 center red-text text-lighten-4"href="index.php">Inscription</a> 
+          <a class="col s3 center red-text text-lighten-4" href="index.php">Connexion</a> <a  class="col s3 center red-text text-lighten-4" href="index.php">Rechercher</a>
+        </nav>
+      </header>
+      <section class="">
+        <form class="">
+        <input class="" type="text" name="Prenom">
+     	<input class="" type="text" name="Nom">
+     	<input class="" type="text" name="Adresse">
+     	<input class="" type="password" name="Mot de passe">
+     	<input class="" type="password" name="remdp">
+     	<label class="">Centre d'interêts:</label>
+     	<label class="">Technologies</label>
+     	<input class="" type="checkbox" name="Technologies">
+     	<label class="">Voyages</label>
+     	<input class="" type="checkbox" name="Voyages">
+     	<label class="">Football</label>
+     	<input class="" type="checkbox" name="Football">
+     	<label class="">Actualitées</label>
+     	<input class="" type="checkbox" name="Actualitées">
+		<input class="" type="submit" name="Valider">
+        </form>
+      </section>
+
+    <footer class="">
+    <div class="card-panel red darken-3">
+      <a href="index.php" class="red-text text-lighten-4">Accueil</a> <a  class="red-text text-lighten-4" href="index.php">Inscription</a> 
+      <a class="red-text text-lighten-4" href="index.php">Connexion</a> <a  class="red-text text-lighten-4" href="index.php">Rechercher</a>
+    </div>
+    </footer>
+    
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    </body>
+
+  </html>
---
+++ jour08/job03/index.php
@@ -0,0 +1,46 @@
+
+ <!DOCTYPE html>
+  <html>
+  
+    <head>
+    <title>Jour08-Job03</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    </head>
+
+    <body>
+      <header class="red darken-1">
+        <nav class="container red darken-3 row">
+          <a href="index.php" class="col s3 center red-text text-lighten-4">Accueil</a> <a  class="col s3 center red-text text-lighten-4"href="index.php">Inscription</a> 
+          <a class="col s3 center red-text text-lighten-4" href="index.php">Connexion</a> <a  class="col s3 center red-text text-lighten-4" href="index.php">Rechercher</a>
+        </nav>
+      </header>
+
+      <section class="">
+        <form class="">
+          <input class="" type="text" name="prenom">
+          <input class="" type="text" name="nom">
+          <input class="" type="text" name="adresse">
+          <input class="" type="password" name="mdp">
+          <input class="" type="password" name="remdp">
+          <label class="">passions:</label>
+          <label class="">informatique</label>
+          <input class="" type="checkbox" name="informatique">
+          <label class="">voyage</label>
+          <input class="" type="checkbox" name="voyages">
+          <label class="">sport</label>
+          <input class="" type="checkbox" name="sport">
+          <label class="">lecture</label>
+          <input class="" type="checkbox" name="lecture">
+        <input class="" type="submit" name="env">
+        </form>
+      </section>
+
+    <footer class="row red darken-1">
+      <a href="index.php" class="col s3 center red-text text-lighten-4">Accueil</a> <a  class="col s3 center red-text text-lighten-4" href="index.php">Inscription</a> 
+      <a class="col s3 center red-text text-lighten-4" href="index.php">Connexion</a> <a  class="col s3 center red-text text-lighten-4" href="index.php">Rechercher</a>
+    </footer>
+    
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    </body>
+
+  </html>
