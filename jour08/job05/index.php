cf04d7191731c857325d2fd83ad553a5a3292f1b
remy-inguanez <55872961+remy-inguanez@users.noreply.github.com>
2020-09-16 17:13:04 +0200

    Add files via upload

---
+++ jour08/job05/index.php
@@ -0,0 +1,152 @@
+
+ <!DOCTYPE html>
+  <html>
+
+<head>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <title>Jour08-Job05</title>
+</head>
+
+    <body class="amber">
+      
+  <nav class="red">
+    <div class="nav-wrapper valign-wrapper">
+      <img class="brand-logo center" src="img/R1.png" width="50">
+      <ul class="right hide-on-med-and-down">
+        <li><a class="yellow-text" href="">Accueil</a></li>
+        <li><a class="yellow-text" href="">About me</a></li>
+        <li><a class="yellow-text" href="">Galerie</a></li>
+        <li><a class="yellow-text" href="">Blog</a></li>
+        <li><a class="amber accent-4 waves-effect waves-light btn">Contact</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <br/>
+  <div class="container amber lighten-4 z-depth-4">
+    <div class="row">
+      <div class="row valign-wrapper">
+      <div class="col s1"></div>
+      <div class="red z-depth-2 card col s2">
+          <div class=" card-image">
+            <br/>
+            <img class="responsive-img" src="img/kenji_893-on-twitter.png" max width="100%" height="auto">
+            <span class="card-title ">Samuraï Goku</span>        
+            <br/>
+          </div>
+      </div>
+      <div class="col s1"></div>
+      <div class="red z-depth-2 card col s4">
+          <div class=" card-image">
+            <br/>
+            <img class="responsive-img" src="img/Guillem Daudén on-Instagram_ “_Ronin Son Gohan.jpg">
+            <span class="card-title ">Young Ronin Gohan</span>    
+            <br/>
+          </div>
+      </div>
+      <div class="col s1"></div>
+        <div class="red z-depth-2 card col s2">
+          <div class=" card-image">
+            <br/>
+            <img class="responsive-img" src="img/Dragon-Ball-version-samouraï!.jpg">
+            <span class="card-title ">Samuraï Vegeta</span>        
+            <br/>
+        </div>
+      </div>
+      <div class="col s1"></div>
+  </div>
+        <div class="row valign-wrapper">
+      <div class="col s1"></div>
+      <div class="red z-depth-2 card col s3">
+          <div class=" card-image">
+            <br/>
+            <img class="responsive-img" src="img/kenji_893 on Twitter (1).png">
+            <span class="card-title ">Ninja Trunks</span>        
+            <br/>
+          </div>
+      </div>
+      <div class="col s1"></div>
+      <div class="red z-depth-2 card col s2">
+          <div class=" card-image">
+            <br/>
+            <img class="responsive-img" src="img/kenji893 - Professional, Digital Artist _ DeviantArt.jpg">
+            <span class="card-title ">Samuraï Piccolo</span>        
+            <br/>
+          </div>
+      </div>
+      <div class="col s1"></div>
+        <div class="red z-depth-2 card col s3">
+          <div class=" card-image">
+            <br/>
+            <img class="responsive-img" src="img/Guillem-Daudén on Instagram_ “_Samurai Bardock.jpg">
+            <span class="card-title ">Savage Samuraï Bardock</span>       
+            <br/>
+        </div>
+      </div>
+      <div class="col s1"></div> 
+  </div>
+        <div class="row valign-wrapper">
+      <div class="col s1"></div>
+      <div class="red z-depth-2 card col s2">
+          <div class=" card-image">
+            <br/>
+            <img class="responsive-img" src="img/kenji_893-on -Twitter(2).png">
+            <span class="card-title ">Samuraï Freezer</span>        
+            <br/>
+          </div>
+      </div>
+      <div class="col s1"></div>
+      <div class="red z-depth-2 card col s4">
+          <div class=" card-image">
+            <br/>
+            <img class="responsive-img" src="img/Celestial Golden Dragon by kenji_893.jpg">
+            <span class="card-title ">Golden Samuraï Goku</span>       
+            <br/>
+          </div>
+      </div>
+      <div class="col s1"></div>
+        <div class="red z-depth-2 card col s2">
+          <div class=" card-image">
+            <br/>
+            <img class="responsive-img" src="img/Bandit-Buu.jpg">
+            <span class="card-title ">Bandit Buu</span>        
+            <br/>
+        </div>
+      </div>
+      <div class="col s1"></div>  
+  </div>
+        <div class="row valign-wrapper">
+      <div class="col s1"></div>
+      <div class="red z-depth-2 card col s3">
+          <div class=" card-image">
+            <br/>
+            <img class="responsive-img" src="img/Savage-Ronin-Broly.jpg">
+            <span class="card-title ">Savage Ronin Broly</span>        
+            <br/>
+          </div>
+      </div>
+      <div class="col s1"></div>
+      <div class="red z-depth-2 card col s2">
+          <div class=" card-image">
+            <br/>
+            <img class="responsive-img" src="img/Samurai-Perfect-Cell _ Dragon -Ball.jpg">
+            <span class="card-title ">Samuraï Cell</span>    
+            <br/>
+          </div>
+      </div>
+      <div class="col s1"></div>
+        <div class="red z-depth-2 card col s3">
+          <div class=" card-image">
+            <br/>
+            <img class="responsive-img" src="img/Samurai-VegetoXGogeta.jpg">
+            <span class="card-title ">Samuraï Vegeto/Gogeta</span>        
+            <br/>
+        </div>
+      </div>
+      <div class="col s1"></div>  
+  </div>
+  </div>
+    </body>
+
+  </html>
